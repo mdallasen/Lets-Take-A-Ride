c1681eef5ea53dd7172f3bb6e21a5d7138a0d7cd
Matthew Dall'Asen <118943691+mdallasen@users.noreply.github.com>
2025-04-16 15:48:34 -0400

    setting up env

--- src/envs/nyc.py
+++ src/envs/nyc.py
@@ -8,11 +8,18 @@ def setup_environment(show_graph = True):
     """
 
     data_folder = os.path.join(os.getcwd(), "src", "data")
-    cache_folder= os.path.join(os.getcwd(), "src", "data")
+    cache_folder = os.path.join(os.getcwd(), "src", "data")
     ox.config(log_console=True, use_cache=True, data_folder=data_folder, cache_folder=cache_folder)
-    map = ox.graph_from_place('Manhattan, New York, USA', network_type='drive')
+    map = ox.graph_from_place('Manhattan, New York, USA', network_type='drive_service')
+    nodes, edges = ox.graph_to_gdfs(map)
     
-    if show_graph == True: 
+    node_features = nodes[['osmid', 'x', 'y', 'street_count', 'degree']]
+    edge_features = edges[['osmid', 'highway', 'length', 'oneway', 'lanes', 'maxspeed', 'traffic_signals', 'surface', 'access']]
+    
+    if show_graph: 
         ox.plot_graph(map)
+
+    return node_features, edge_features
+    
 
 setup_environment()
