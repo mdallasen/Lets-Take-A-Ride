b5aacdafdf8c0759838c82bab6f03ba44c6c6823
Matthew Dall'Asen <118943691+mdallasen@users.noreply.github.com>
2025-04-16 15:30:31 -0400

    seting up env

--- src/envs/nyc.py
+++ src/envs/nyc.py
@@ -1,8 +1,18 @@
 import osmnx as ox 
 import os 
 
-data_folder = os.path.join(os.getcwd(), "src", "data")
-ox.config(log_console=True, use_cache=True, data_folder=data_folder)
-map = ox.graph_from_place('Manhattan, New York, USA', network_type='drive')
+def setup_environment(show_graph = True):
+    """
+    Set up the environment for OSMnx to work with OpenStreetMap data.
+    This function configures the data folder and initializes OSMnx settings.
+    """
 
-ox.plot_graph(map)
+    data_folder = os.path.join(os.getcwd(), "src", "data")
+    cache_folder= os.path.join(os.getcwd(), "src", "data")
+    ox.config(log_console=True, use_cache=True, data_folder=data_folder, cache_folder=cache_folder)
+    map = ox.graph_from_place('Manhattan, New York, USA', network_type='drive')
+    
+    if show_graph == True: 
+        ox.plot_graph(map)
+
+setup_environment()
