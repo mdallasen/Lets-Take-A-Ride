7ad93eaf721a47ee1f7edb95c04c5fdaf8f498d3
Matthew Dall'Asen <118943691+mdallasen@users.noreply.github.com>
2025-04-20 13:32:38 -0400

    DQN model

--- src/model/DQN.py
+++ src/model/DQN.py
@@ -5,14 +5,42 @@ from tensorflow.keras import layers
     # Make this more complex
 
 class DQN(tf.keras.Model):
-    def __init__(self, state_size, action_size): 
+    def __init__(self, state_size, num_actions): 
         self.state_size = state_size
-        self.dense1 = layers.Dense(128, activation = 'relu')
-        self.dense2 = layers.Dense(64, activation = 'relu')
-        self.output = layers.Dense(action_size, activation = 'linear')
+        self.num_actions = num_actions
+        
+        self.model = tf.keras.Sequential(
+            [
+                tf.keras.layers.Dense(64, activation = 'relu', input_shape = (state_size,)),
+                tf.keras.layers.Dense(32, activation = 'relu'), 
+                tf.keras.layers.Dense(num_actions, activations = None)  
+            ]
+        )
 
-    def call(self, inputs): 
-        x = tf.one_hot(inputs, depth = self.state_size)
-        x = tf.cast(x, tf.float32)
-        x = self.dense1(x)
-        x = self.dense2(x)
+        self.model.optimizer = tf.keras.optimizer.Adam(learning_rate = 0.001) 
+        self.model.build(input_shape = (None, state_size))
+        self.target_model = tf.keras.models.clone_modle(self.model)
+        self.target_model.set_weights(self.model.get_weights())
+
+    def call(self, states): 
+        
+        self.model(states)
+
+    def loss_func(self, batch, discount_factor = 0.99): 
+
+        states, actions, rewards, next_states, done = batch 
+
+        states = tf.convert_to_tensor(states, dtype=tf.float32)
+        actions = tf.convert_to_tensor(actions, dtype=tf.int32)
+        rewards = tf.convert_to_tensor(rewards, dtype=tf.float32)
+        next_states = tf.convert_to_tensor(next_states, dtype=tf.float32)
+        done = tf.convert_to_tensor(done, dtype=tf.float32)
+
+        q_s = self(states)
+        q_s_a = tf.reduce_sum(q_s * tf.one_hot(actions, self.num_actions), axis=1)
+        q_next_s = self.target_model(next_states)
+        q_next_s_a = tf.reduce_max(q_next_s, axis=1)
+
+        loss = tf.reduce_mean(tf.square(q_s_a - (rewards + discount_factor * (1 - done) * q_next_s_a)))
+
+        return loss
